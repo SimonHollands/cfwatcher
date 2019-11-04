c30c921d2c3928215d967992a03d9ca718fc1e8f
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-04 15:49:56 -0800

    start

---
+++ api.py
@@ -0,0 +1,50 @@
+import flask
+from flask import request, jsonify
+from flask import send_file
+from Surf_counter.model_server import ServeModel
+import urllib.request
+import os   
+from flask import Flask, session
+from flask_session.__init__ import Session
+
+#######
+##Description for the app
+#######
+
+app_model='yolo.h5'
+
+model_links={'yolo-tiny.h5':'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo-tiny.h5',
+             'yolo.h5': 'https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5'}
+
+model_path = "./models/" + app_model 
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# SESSION_TYPE = 'redis'
+# app.config.from_object(__name__)
+# Session(app)
+
+
+print("Read Model")
+urllib.request.urlretrieve(model_links[app_model], model_path)
+sv=ServeModel(app_model)
+
+@app.route('/model')
+def api_model():
+    '''Grab predictions from model server '''
+    print("Here  starting Api Model ! ")
+    count=sv.serveit()
+    return str(count)
+
+@app.route('/', methods=['GET'])
+def home():
+    print("Here  at home ! ")
+
+    '''Home page'''
+    return '''<h1>Surfing a Deep Learning Model</h1>
+<p>A Yolo Model for detecting surfers.</p>'''
+
+if __name__ == '__main__':
+    #app.run(threaded=False,use_reloader=False, host="0.0.0.0", port=80)
+    app.run(threaded=False,use_reloader=False)
