f86bc08ad45a705325e2d60150fbd2e9c2b1f7a9
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-04 18:48:30 -0800

    Change interval

--- api.py
+++ api.py
@@ -34,7 +34,7 @@ def fetch_data_from_api():
 
 @app.route('/')
 def home():
-    message=datetime.date.today().strftime("%B %d, %Y")
+    message=str(datetime.datetime.now())
     return render_template('index.html',message=message)
 
 if __name__ == '__main__':
@@ -42,7 +42,7 @@ if __name__ == '__main__':
 
     #add your job here 
     #app.run(threaded=False,use_reloader=False, host="0.0.0.0", port=80)
-    scheduler.add_job(func=fetch_data_from_api, trigger="interval", seconds=30)
+    scheduler.add_job(func=fetch_data_from_api, trigger="interval", seconds=60*8)
     scheduler.start()
 
     app.run(threaded=False,use_reloader=False)
