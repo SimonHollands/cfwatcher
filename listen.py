f95ed480201019a133eb746280836c4a52f9106a
simonhollands <hollands@Simons-MacBook-Pro.local>
2019-11-04 17:15:07 -0800

    Collecting Data Locally

---
+++ listen.py
@@ -0,0 +1,40 @@
+from os import listdir
+from os.path import isfile, join
+import os, shutil 
+from s3pushpull2 import s3pushpull
+import urllib.request
+import pandas as pd
+import io
+import time
+
+#Something
+s3=s3pushpull()
+#df1=pd.DataFrame([{'a':1,'b':2}])
+#s3.pandas_to_s3(df=df1, bucket='surfcounter14367', key='S3:/testeroo.csv')
+
+class Listener:
+
+    def __init__(self, s3_data_key='S3:/breakwater_data/breakwater_data.csv'):
+        self.vars=['date','surfer_count']
+        self.s3_data_key=s3_data_key
+        self.obj=s3.s3.get_object(Bucket='surfcounter14367', Key=s3_data_key)
+        self.data = pd.read_csv(io.BytesIO(self.obj['Body'].read()))[self.vars]
+
+    def hit_model(self):
+        response = urllib.request.urlopen('http://13.57.217.48/model').read().decode('ASCII')
+        new = [{'date':pd.datetime.now(),'surfer_count':int(response)}]
+        self.data=self.data.append(pd.DataFrame(new))
+        self.count=int(response)
+        self.data.to_csv('data/temp.csv')
+        s3.upload_aws('data/temp.csv', 'S3:/breakwater_data/breakwater_data.csv')
+        print(self.data.head())
+
+
+if __name__ == "__main__":
+    listener=Listener()
+    print(listener.data.head())
+
+    for i in range(100):
+        listener.hit_model()
+        print(f'''Smack, {listener.count} surfers right now.''')
+        time.sleep(60)
